6b7da58783374922f95adacef26ec70f452f8b57
minhkhang1008 <minhkhang.0884@gmail.com>
2025-04-22 00:36:15 +0700

    Questions modifying available

--- data/presets.js
+++ data/presets.js
@@ -4,19 +4,19 @@ export const PRESETS = {
         id: "GDCD9-q1",
         prompt: "Vi phạm pháp luật là gì và các loại?",
         answerKey:
-          "Vi phạm pháp luật là hành vi trái pháp luật, có lỗi do người có năng lực trách nhiệm pháp lí thực hiện, xâm hại các quan hệ xã hội được pháp luật bảo vệ."
+          "Vi phạm pháp luật là hành vi trái pháp luật, có lỗi do người có năng lực trách nhiệm pháp lí thực hiện, xâm hại các quan hệ xã hội được pháp luật bảo vệ. Vi phạm pháp luật là hành vi trái pháp luật, có lỗi do người có năng lực trách nhiệm pháp lí thực hiện, xâm hại các quan hệ xã hội được pháp luật bảo vệ.\nCác loại vi phạm pháp luật bao gồm: vi phạm hình sự, vi phạm hành chính, vi phạm dân sự, vi phạm kỉ luật."
       },
       {
         id: "GDCD9-q2",
         prompt: "Trách nhiệm pháp lí là gì và các loại?",
         answerKey:
-          "Trách nhiệm pháp lí là nghĩa vụ mà các cá nhân hoặc tổ chức phải gánh chịu hậu quả bất lợi do nhà nước quy định từ hành vi vi phạm pháp luật của mình."
+          "Trách nhiệm pháp lí là nghĩa vụ mà các cá nhân hoặc tổ chức phải gánh chịu hậu quả bất lợi do nhà nước quy định từ hành vi vi phạm pháp luật của mình. Các loại trách nhiệm pháp lí bao gồm: trách nhiệm hình sự, trách nhiệm hành chính, trách nhiệm kỉ luật, trách nhiệm dân sự"
       },
       {
         id: "GDCD9-q3",
         prompt: "Ý nghĩa của trách nhiệm pháp lí?",
         answerKey:
-          "Ý nghĩa của trách nhiệm pháp lí:\n- Giúp ngăn ngừa, giáo dục, cải tạo đối với những hành vi vi phạm pháp luật.\n- Giúp giáo dục mọi người có ý thức tôn trọng, chấp hành đúng theo quy định của pháp luật."
+          "Giúp ngăn ngừa, giáo dục, cải tạo đối với những hành vi vi phạm pháp luật. Giúp giáo dục mọi người có ý thức tôn trọng, chấp hành đúng theo quy định của pháp luật."
       }
     ]
   };
